566241e6623130ff7b5af06016f2f90ddfae447d
Flopy02 <flo.barege@gmail.com>
2024-11-21 15:39:25 +0100

    texture Terre

--- src/Earth.java
+++ src/Earth.java
@@ -1,4 +1,25 @@
-package PACKAGE_NAME;
+import javafx.scene.Group;
+import javafx.scene.image.Image;
+import javafx.scene.paint.PhongMaterial;
+import javafx.scene.shape.Sphere;
 
-public class Earth {
+public class Earth extends Group {
+    private Sphere sph;
+
+    public Earth() {
+        // Création de la sphère avec un rayon de 300 pixels
+        sph = new Sphere(300);
+
+        // Création d'un matériau pour appliquer une texture
+        PhongMaterial material = new PhongMaterial();
+
+        // Chargement de l'image de la texture avec un chemin absolu
+        material.setDiffuseMap(new Image("file:/C:/Users/floba/IdeaProjects/Projet_JAVA_S9/src/data/earth_lights_4800.png"));
+
+        // Application du matériau à la sphère
+        sph.setMaterial(material);
+
+        // Ajout de la sphère au groupe
+        this.getChildren().add(sph);
+    }
 }
--- src/Interface.java
+++ src/Interface.java
@@ -1,18 +1,48 @@
 import javafx.application.Application;
-import javafx.scene.Group;
+import javafx.scene.PerspectiveCamera;
 import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.input.ScrollEvent;
 import javafx.stage.Stage;
-import javafx.scene.layout.Pane;
+
 public class Interface extends Application {
     @Override
-    public void start(Stage primaryStage) throws Exception{
-        primaryStage.setTitle("Hello world");
-        Group root = new Group();
-        Pane pane = new Pane(root);
-        Scene theScene = new Scene(pane, 600, 400,true);
+    public void start(Stage primaryStage) throws Exception {
+        // Configuration de la fenêtre
+        primaryStage.setTitle("Textured Earth");
+
+        // Création de l'objet Earth (sphère avec texture)
+        Earth earth = new Earth();
+
+        // Création d'une caméra
+        PerspectiveCamera camera = new PerspectiveCamera(true);
+        camera.setTranslateZ(-1000); // Position initiale de la caméra
+        camera.setNearClip(0.1);     // Distance minimale visible
+        camera.setFarClip(2000.0);   // Distance maximale visible
+        camera.setFieldOfView(35);   // Champ de vision
+
+        // Création de la scène
+        Scene theScene = new Scene(earth, 800, 600, true);
+        theScene.setCamera(camera);
+
+        // Gestion du zoom et dézoom avec la molette
+        theScene.addEventHandler(ScrollEvent.SCROLL, event -> {
+            double deltaY = event.getDeltaY(); // Valeur du défilement de la molette
+            camera.setTranslateZ(camera.getTranslateZ() + deltaY * 0.1); // Ajuste la position Z
+        });
+
+        // Gestion des clics pour afficher les coordonnées
+        theScene.addEventHandler(MouseEvent.ANY, event -> {
+            if (event.getEventType() == MouseEvent.MOUSE_PRESSED) {
+                System.out.println("Clicked on: X=" + event.getSceneX() + ", Y=" + event.getSceneY());
+            }
+        });
+
+        // Ajout de la scène à la fenêtre
         primaryStage.setScene(theScene);
         primaryStage.show();
     }
+
     public static void main(String[] args) {
         launch(args);
     }
